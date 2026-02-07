df897e75a09257fa5e6ff35ceff1ffd9f7d1afdc
vaheh <vahe.h.x@gmail.com>
2026-02-07 15:18:45 +0400

    initial project

---
+++ src/main/java/com/example/auction/adauctionservice/AdAuctionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.auction.adauctionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdAuctionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdAuctionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/auction/adauctionservice/AdAuctionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auction.adauctionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdAuctionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
